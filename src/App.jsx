6cfb0c0b5a0fd4aef754747c2f24cd94e48f81e3
Coderganesh <gsb021@nist.edu>
2024-01-19 21:47:45 +0530

    authentication setupu

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  console.log(process.env.REACT_APP_APPWRITE_URL)
+  return (
+   <>
+   <h1>Hello World</h1>
+   </>
+  );
+}
+
+export default App;
